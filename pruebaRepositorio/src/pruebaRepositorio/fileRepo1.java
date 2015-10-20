8b1795d95400ce315d91b3e1d3b97b5db414b671
21438255 <21438255@N204-05.alumnos.uem.es>
2015-10-20 09:44:35 +0200

    cambio en rama borja

--- pruebaRepositorio/src/pruebaRepositorio/fileRepo1.java
+++ pruebaRepositorio/src/pruebaRepositorio/fileRepo1.java
@@ -4,7 +4,8 @@ public class fileRepo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola2");
+		for(int i = 0; i < 5; i++){
+			System.out.println("hola2");
+		}
 	}
-
 }
